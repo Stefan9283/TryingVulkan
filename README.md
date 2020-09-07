b448d334f1205f547e959e8bb2098173b820a821
Stefan9283 <stefan9283@gmail.com>
2020-09-07 18:56:13 +0300

    Manual Commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # TryingVulkan
 
+SRC:
+
+https://vulkan-tutorial.com/
+
 TODO:
 - ~~Fix CMake linking issues~~
 - ~~layer validation fixing~~
--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-rm -r CMakeFiles CMakeCache.txt *.vcxproj* *.sln cmake_install* x64 VULKAN* Debug Makefile
+rm -r CMakeFiles CMakeCache.txt *.vcxproj* *.sln cmake_install* x64 VULKAN* Debug Makefile Release out
