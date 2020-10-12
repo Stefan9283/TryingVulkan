8b550800b333353ad49f7fd94febf72489bf3412
Ștefan Toma <34220105+Stefan9283@users.noreply.github.com>
2020-10-12 15:42:00 +0300

    modified clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-rm -r CMakeFiles CMakeCache.txt *.vcxproj* *.sln cmake_install* x64 VULKAN* Debug Makefile Release out
+rm -r CMakeFiles CMakeCache.txt *.vcxproj* *.sln cmake_install* x64 VULKAN* Debug Makefile Release out .vs
